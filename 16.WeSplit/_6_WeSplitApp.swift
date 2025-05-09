713cd23b3a9b9e1e447d018146a8d1186ce708ca
Valentin <latronixo@gmail.com>
2025-05-09 14:57:41 +0300

    initialCommit

---
+++ 16.WeSplit/_6_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6_WeSplitApp.swift
+//  16.WeSplit
+//
+//  Created by Валентин on 09.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct _6_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
