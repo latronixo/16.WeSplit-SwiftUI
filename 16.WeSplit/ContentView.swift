9b8df2f7dc26eb7ffc9ccd2a9b3f6598e2354592
Valentin <latronixo@gmail.com>
2025-05-10 17:33:01 +0300

    Добавил навстек и пикер для выбора количества гостей

--- 16.WeSplit/ContentView.swift
+++ 16.WeSplit/ContentView.swift
@@ -15,45 +15,25 @@ struct ContentView: View {
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Сумма", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    .keyboardType(.decimalPad)
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Сумма", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Количество гостей", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) гостей")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                Section {
+                    TextField("Сумма", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                }
             }
-            Section {
-                TextField("Сумма", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    .keyboardType(.decimalPad)
-//                TextField("Сумма", text: Binding(
-//                    get: {
-//                        // Преобразуем Double в строку в местной валюте
-//                        checkAmount.formatted(.currency(code: Locale.current.currency?.identifier ?? "USD"))
-//                    },
-//                    set: { newValue in
-//                        // Получаем символ местной валюты
-//                        let currencySymbol = Locale.current.currencySymbol ?? "$"
-//                        // Преобразуем строку обратно в Double
-//                        if let value = Double(newValue.replacingOccurrences(of: currencySymbol, with: "").trimmingCharacters(in: .whitespacesAndNewlines)) {
-//                            checkAmount = value
-//                        }
-//                    }
-//                ))
-//                .keyboardType(.decimalPad) // Удобная клавиатура для ввода чисел
-//                TextField("Сумма", text: Binding(
-//                    get: {
-//                        // Преобразуем Double в строку в местной валюте
-//                        checkAmount.formatted(.currency(code: Locale.current.currency?.identifier ?? "USD"))
-//                    },
-//                    set: { newValue in
-//                        // Получаем символ местной валюты
-//                        let currencySymbol = Locale.current.currencySymbol ?? "$"
-//                        // Преобразуем строку обратно в Double
-//                        if let value = Double(newValue.replacingOccurrences(of: currencySymbol, with: "").trimmingCharacters(in: .whitespacesAndNewlines)) {
-//                            checkAmount = value
-//                        }
-//                    }
-//                ))
-//                .keyboardType(.decimalPad)
-            }
+            .navigationTitle("Чаевые поровну")
         }
     }
 }
